791bc39cf6ad824afc25a40d55e75b2f6a7c1ef9
Max Mannstein <max.mannstein@gmail.com>
2022-07-05 16:50:36 +0200

    durchschnitt

--- NotenApp/NotenApp/Pages/Halbjahr2.xaml.cs
+++ NotenApp/NotenApp/Pages/Halbjahr2.xaml.cs
@@ -15,16 +15,31 @@ namespace NotenApp.Pages
     public partial class Halbjahr2 : ContentPage
     {
         Halbjahr2ViewModel _model;
+        public List<float?> durchschnitte = new List<float?>();
+        public float? count= 0;
+        public float? gesamtDurchschnitt;
         public Halbjahr2()
         {
 
             InitializeComponent();
             _model = BindingContext as Halbjahr2ViewModel;
+            
         }
         protected async override void OnAppearing()
         {
             base.OnAppearing();
             await _model.Refresh();
+            foreach (Halbjahr2Model fach in _model.FaecherHJ2)
+            {
+                durchschnitte.Add(fach.Durchschnitt);
+            }
+            for (int i = 0; i < durchschnitte.Count; i++)
+            {
+                count += durchschnitte[i];
+            }
+            gesamtDurchschnitt = count / durchschnitte.Count;
+            h.Text = gesamtDurchschnitt.ToString();
+            count = 0;
         }
 
         private async void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
